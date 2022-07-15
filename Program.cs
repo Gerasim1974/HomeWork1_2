6cf903c191648cff5a2e1ece175bc9ed847f3eb5
ПК <GIS_1974@tut.by>
2022-07-15 12:44:50 +0300

    HomeWork1 Task2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter start number");
+            string? sStartNumber = Console.ReadLine();
+            int nStartNumber = int.Parse(sStartNumber);
+            Console.WriteLine("please enter end number");
+            string? sEndNumber = Console.ReadLine();
+            int nEndNumber = int.Parse(sEndNumber);
+            Console.WriteLine("Result:");
+            while (nStartNumber <= nEndNumber)
+            {
+                Console.WriteLine(nStartNumber);
+                nStartNumber++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
